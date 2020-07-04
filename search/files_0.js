4e274d39c87815af688cca36d6feee18cde2cfb1
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2020-07-04 11:31:52 +0000

    Deploying to gh-pages from  @ fd0a27b9dc9e70224d5a5505402f05fb71cf8756 🚀

--- search/files_0.js
+++ search/files_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['unicode_5fchar_2eh_19',['unicode_char.h',['../unicode__char_8h.html',1,'']]]
+  ['unicode_5fchar_2eh_20',['unicode_char.h',['../unicode__char_8h.html',1,'']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,15 +1,15 @@
 var searchData=
 [
-  ['getbidirectionalcategory_23',['getBidirectionalCategory',['../unicode__char_8h.html#a26834c8a76773a48368a8f52e0c29270',1,'unicode']]],
-  ['getcanonicalcombiningclass_24',['getCanonicalCombiningClass',['../unicode__char_8h.html#a2a5adca11cda8f424b8c70f547bce434',1,'unicode']]],
-  ['getdecimaldigitvalue_25',['getDecimalDigitValue',['../unicode__char_8h.html#aec796d85bd29e2f5d43e11ef5d98a344',1,'unicode']]],
-  ['getdecompositionmapping_26',['getDecompositionMapping',['../unicode__char_8h.html#a647a8e614fa24c6e50805850f968b145',1,'unicode::getDecompositionMapping(UChar code)'],['../unicode__char_8h.html#a80339e398544e6930ec003dfc508aa05',1,'unicode::getDecompositionMapping(UChar code, UChar buffer[])']]],
-  ['getdecompositionmappingtag_27',['getDecompositionMappingTag',['../unicode__char_8h.html#aa800b8f8db082e4047320225c1721f85',1,'unicode']]],
-  ['getdigitvalue_28',['getDigitValue',['../unicode__char_8h.html#a9ed6fb7ce7aeadc1994bbff9717e0f04',1,'unicode']]],
-  ['getgeneralcategory_29',['getGeneralCategory',['../unicode__char_8h.html#a1adec98b600f3addb8e0be3929c0adb7',1,'unicode']]],
-  ['getlowercase_30',['getLowerCase',['../unicode__char_8h.html#a74d728b8a2336ff918abdd79a01c4e86',1,'unicode']]],
-  ['getnumericfraction_31',['getNumericFraction',['../unicode__char_8h.html#a8dd9a6fe2c5b693e705aeac0d0a3750c',1,'unicode']]],
-  ['getnumericvalue_32',['getNumericValue',['../unicode__char_8h.html#ad8c4cd29d5c04d1dc774f5e6b81fb9b7',1,'unicode']]],
-  ['gettitlecase_33',['getTitleCase',['../unicode__char_8h.html#af31c9ca791f50c8ca3c9f18953a941c6',1,'unicode']]],
-  ['getuppercase_34',['getUpperCase',['../unicode__char_8h.html#a827a1de593760ef2ed4536b416fdbce9',1,'unicode']]]
+  ['getbidirectionalcategory_24',['getBidirectionalCategory',['../unicode__char_8h.html#a26834c8a76773a48368a8f52e0c29270',1,'unicode']]],
+  ['getcanonicalcombiningclass_25',['getCanonicalCombiningClass',['../unicode__char_8h.html#a2a5adca11cda8f424b8c70f547bce434',1,'unicode']]],
+  ['getdecimaldigitvalue_26',['getDecimalDigitValue',['../unicode__char_8h.html#aec796d85bd29e2f5d43e11ef5d98a344',1,'unicode']]],
+  ['getdecompositionmapping_27',['getDecompositionMapping',['../unicode__char_8h.html#a647a8e614fa24c6e50805850f968b145',1,'unicode::getDecompositionMapping(UChar code)'],['../unicode__char_8h.html#a80339e398544e6930ec003dfc508aa05',1,'unicode::getDecompositionMapping(UChar code, UChar buffer[])']]],
+  ['getdecompositionmappingtag_28',['getDecompositionMappingTag',['../unicode__char_8h.html#aa800b8f8db082e4047320225c1721f85',1,'unicode']]],
+  ['getdigitvalue_29',['getDigitValue',['../unicode__char_8h.html#a9ed6fb7ce7aeadc1994bbff9717e0f04',1,'unicode']]],
+  ['getgeneralcategory_30',['getGeneralCategory',['../unicode__char_8h.html#a1adec98b600f3addb8e0be3929c0adb7',1,'unicode']]],
+  ['getlowercase_31',['getLowerCase',['../unicode__char_8h.html#a74d728b8a2336ff918abdd79a01c4e86',1,'unicode']]],
+  ['getnumericfraction_32',['getNumericFraction',['../unicode__char_8h.html#a8dd9a6fe2c5b693e705aeac0d0a3750c',1,'unicode']]],
+  ['getnumericvalue_33',['getNumericValue',['../unicode__char_8h.html#ad8c4cd29d5c04d1dc774f5e6b81fb9b7',1,'unicode']]],
+  ['gettitlecase_34',['getTitleCase',['../unicode__char_8h.html#af31c9ca791f50c8ca3c9f18953a941c6',1,'unicode']]],
+  ['getuppercase_35',['getUpperCase',['../unicode__char_8h.html#a827a1de593760ef2ed4536b416fdbce9',1,'unicode']]]
 ];
